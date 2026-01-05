a952e998f9d50d98bf81a0451487e2a9dd678bad
coder-nitin07 <nitinthakur87504@gmail.com>
2026-01-05 12:07:57 +0530

    setup order api and gateway

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const app = express();
+const axios = require("axios");
 
 app.use(express.json());
 
 app.get("/health", (req, res) => {
   res.json({ service: "API Gateway", status: "OK" });
+});
+
+app.post("/orders", async (req, res) => {
+  try {
+    const response = await axios.post(
+      "http://localhost:3001/orders",
+      req.body
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "Order service unavailable" });
+  }
 });
 
 app.listen(3000, () => {
--- order-service/server.js
+++ order-service/server.js
@@ -7,6 +7,15 @@ app.get("/health", (req, res) => {
   res.json({ service: "Order Service", status: "OK" });
 });
 
+app.post("/orders", (req, res) => {
+  console.log("Order Service received order:", req.body);
+
+  res.json({
+    message: "Order created",
+    order: req.body,
+  });
+});
+
 app.listen(3001, () => {
   console.log("Order Service running on port 3001");
 });
