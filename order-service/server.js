45ec7a5d232ca4076015fb926682051875f531d2
coder-nitin07 <nitinthakur87504@gmail.com>
2026-01-05 12:34:34 +0530

    setup payment API and update order api

--- order-service/server.js
+++ order-service/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const axios = require("axios");
 
 app.use(express.json());
 
@@ -7,13 +8,25 @@ app.get("/health", (req, res) => {
   res.json({ service: "Order Service", status: "OK" });
 });
 
-app.post("/orders", (req, res) => {
+app.post("/orders", async (req, res) => {
   console.log("Order Service received order:", req.body);
 
-  res.json({
-    message: "Order created",
-    order: req.body,
-  });
+  try {
+    const paymentResponse = await axios.post(
+      "http://localhost:3002/payments",
+      {
+        orderAmount: req.body.price,
+        orderId: 'ORD_' + Date.now(),
+      }
+    );
+
+    res.json({
+      message: "Order created",
+      payment: paymentResponse.data
+    });
+  } catch (err) {
+    res.status(500).json({ error: 'Payment Failed' });
+  }
 });
 
 app.listen(3001, () => {
--- payment-service/server.js
+++ payment-service/server.js
@@ -7,6 +7,16 @@ app.get("/health", (req, res) => {
   res.json({ service: "Payment Service", status: "OK" });
 });
 
+app.post("/payments", (req, res) => {
+  console.log("Payment Service received:", req.body);
+
+  // simulate payment success
+  res.json({
+    status: "SUCCESS",
+    paymentId: "PAY_" + Date.now(),
+  });
+});
+
 app.listen(3002, () => {
   console.log("Payment Service running on port 3002");
 });
